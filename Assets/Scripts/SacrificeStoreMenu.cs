d6c160e4235f8ad5aaf567d4654e9f7638d0a565
Unknown <thunderkerrigan@gmail.com>
2018-12-01 20:08:13 +0100

    #### Musique
    * add both Music from JeuneGen
    
    #### Sacrifice Store
    * WIP
    * can spawn and show sacrifice available

---
+++ Assets/Scripts/SacrificeStoreMenu.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SacrificeStoreMenu : MonoBehaviour
+{
+    public GameObject storePanel;
+    public SimpleObjectPool buttonObjectPool;
+
+    private List<SacrificeMenuButton> buttons = new List<SacrificeMenuButton>();
+    // private List<Power> = [];
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void showMenu()
+    {
+        storePanel.SetActive(true);
+int checkpoint = PlayerPrefs.GetInt("reachedCheckpoints", 1);
+createMenu(checkpoint);
+    }
+
+    public void hideMenu()
+    {
+        storePanel.SetActive(false);
+        
+
+    }
+
+    void createMenu(int checkpoint)
+    {
+        if (checkpoint > buttons.Count)
+        {
+            GameObject newButton = buttonObjectPool.GetObject();
+            newButton.transform.SetParent(storePanel.transform);
+            newButton.transform.localScale = storePanel.transform.localScale;
+            newButton.transform.SetPositionAndRotation(new Vector3(0,0), Quaternion.identity);
+            Sacrifice sacrifice = new Speed();
+            SacrificeMenuButton sacrificeMenuButton = newButton.GetComponent<SacrificeMenuButton>();
+            buttons.Add(sacrificeMenuButton);
+            sacrificeMenuButton.Setup(sacrifice);
+        }
+    }
+
+
+}
